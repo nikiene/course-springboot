c011bd0ebd38a465515b372ffb5721d6984cd614
Nikobafos <nikolasnovo03@gmail.com>
2021-10-01 16:05:47 -0300

    User entity and resource

---
+++ src/main/java/com/kinas/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.kinas.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kinas.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Kinas", "kinas@gmail.com", "11957936055", "asdfqwe1");
+		return ResponseEntity.ok().body(u);
+	}
+}
