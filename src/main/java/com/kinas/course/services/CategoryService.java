0047f35b9edda5251b5b350d12b81785cf3193fc
Nikobafos <nikolasnovo03@gmail.com>
2021-10-04 15:07:38 -0300

    Category

---
+++ src/main/java/com/kinas/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.kinas.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kinas.course.entities.Category;
+import com.kinas.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
