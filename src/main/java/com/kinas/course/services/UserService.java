114a3b3529250dd503b65e0a40ab64bff21c6b03
Nikobafos <nikolasnovo03@gmail.com>
2021-10-06 09:06:12 -0300

    Exception handling - findById

--- src/main/java/com/kinas/course/services/UserService.java
+++ src/main/java/com/kinas/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.kinas.course.entities.User;
 import com.kinas.course.repositories.UserRepository;
+import com.kinas.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
